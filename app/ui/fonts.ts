3ef5dd536ff78424c7486b26153021474147ca70
Donald Roman <romanunuose@gmail.com>
2024-01-07 00:21:07 +0100

    Added fonts and type definitions

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from 'next/font/google'
+import { Hind } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const hind = Hind({ weight: ['300', '400', '500'], subsets: ['latin'] })
