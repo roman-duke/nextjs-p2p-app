48950fb53d3becb26bdc7cc8cb6d910ab7007a77
Donald Roman <romanunuose@gmail.com>
2024-01-09 13:03:49 +0100

    Updated the initial data (now includes wallet and activeUser) to prepopulate the database

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,9 +1,63 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
-    password: '123456',
+    name: 'Alex Iwobi',
+    email: 'alex@iwobi.com',
+    password: 'alexfulham2023',
+  },
+  {
+    id: '410544b2-4105-4271-9855-fec4b6a6442a',
+    name: 'Wilfred Ndidi',
+    email: 'wilfred@ndidi.com',
+    password: 'leicester2023',
+  },
+  {
+    id: '410544b2-4021-4271-9855-fec4b6a6442a',
+    name: 'Ademola Lookman',
+    email: 'ademola@lookman.com',
+    password: 'atalanta2023',
+  },
+  {
+    id: '410544b2-4100-4271-9855-fec4b6a6442a',
+    name: 'Victor Osimhen',
+    email: 'victor@osimhen.com',
+    password: 'victornapoli',
+  },
+  {
+    id: '410544b2-4505-4271-9855-fec4b6a6442a',
+    name: 'Calvin Bassey',
+    email: 'calvin@bassey.com',
+    password: 'defenderfulham',
+  },
+  {
+    id: '410544b2-4390-4271-9855-fec4b6a6442a',
+    name: 'Kelechi Iheanacho',
+    email: 'kelechi@iheanacho.com',
+    password: 'leicesterman',
+  },
+  {
+    id: '410544b2-4600-4271-9855-fec4b6a6442a',
+    name: 'Troost Ekong',
+    email: 'troost@ekong.com',
+    password: 'dinnerman',
+  },
+  {
+    id: '410544b2-4900-4271-9855-fec4b6a6442a',
+    name: 'Taiwo Awoniyi',
+    email: 'taiwo@awoniyi.com',
+    password: 'nottingham-forest',
+  },
+  {
+    id: '410544b2-4980-4271-9855-fec4b6a6442a',
+    name: 'Ahmed Musa',
+    email: 'ahmed@musa.com',
+    password: 'kano-man',
+  },
+  {
+    id: '410544b2-4783-4271-9855-fec4b6a6442a',
+    name: 'Samuel Chukwueze',
+    email: 'samuel@chukwueze.com',
+    password: 'milan-man',
   },
 ];
 
@@ -70,6 +124,15 @@ const customers = [
   },
 ];
 
+const activeUser = [
+  {
+    active_id: customers[0].id,
+    name: 'Alex Iwobi',
+    email: 'alex@iwobi.com',
+    image_url: customers[0].image_url,
+  }
+];
+
 const invoices = [
   {
     customer_id: customers[0].id,
@@ -117,7 +180,7 @@ const invoices = [
     customer_id: customers[3].id,
     amount: 600,
     status: 'paid',
-    date: '2024-03-06',
+    date: '2024-01-06',
   },
   {
     customer_id: customers[4].id,
@@ -163,6 +226,60 @@ const invoices = [
   },
 ];
 
+const wallets = [
+  {
+    customer_id: customers[0].id,
+    amount: 150000,
+    date: '2023-06-05',
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 150000,
+    date: '2022-08-05',
+  },
+  {
+    customer_id: customers[2].id,
+    amount: 150000,
+    date: '2023-09-05',
+  },
+  {
+    customer_id: customers[3].id,
+    amount: 150000,
+    date: '2023-10-05',
+  },
+  {
+    customer_id: customers[4].id,
+    amount: 150000,
+    date: '2023-11-05',
+  },
+  {
+    customer_id: customers[5].id,
+    amount: 150000,
+    date: '2023-05-05',
+  },
+  {
+    customer_id: customers[6].id,
+    amount: 150000,
+    date: '2023-12-05',
+  },
+  {
+    customer_id: customers[7].id,
+    amount: 150000,
+    date: '2024-01-02',
+  },
+  {
+    customer_id: customers[8].id,
+    amount: 150000,
+    date: '2024-01-01',
+  },
+  {
+    customer_id: customers[9].id,
+    amount: 150000,
+    date: '2024-01-07',
+  },
+  
+]
+
 const revenue = [
   { month: 'Jan', revenue: 2000 },
   { month: 'Feb', revenue: 1800 },
@@ -180,7 +297,9 @@ const revenue = [
 
 module.exports = {
   users,
+  activeUser,
   customers,
   invoices,
+  wallets,
   revenue,
 };
